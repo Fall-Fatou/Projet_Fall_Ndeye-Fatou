a63812aa7c7c79c0e79f426ea9e5d2a754d3b96c
Fall Fatou <ndeye-fatou.fall@epita.fr>
2025-11-28 11:56:10 +0100

    first commit

---
+++ src/main/java/org/simplecash/projet_fall_ndeyefatou/ProjetFallNdeyeFatouApplication.java
@@ -0,0 +1,13 @@
+package org.simplecash.projet_fall_ndeyefatou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetFallNdeyeFatouApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetFallNdeyeFatouApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/simplecash/projet_fall_ndeyefatou/ProjetFallNdeyeFatouApplicationTests.java
@@ -0,0 +1,13 @@
+package org.simplecash.projet_fall_ndeyefatou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetFallNdeyeFatouApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
